9cb054e024816d8eda9a5aeaa1ac8629c0acdd93
Dwight Nodolf <dnodolf@gmail.com>
2020-06-29 03:07:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,13 @@
+#returns a string without any additional fomatting when given a 1-element array
 def oxford_comma(array)
   array.join(", ")
   if array.length <= 2 
-    return array.join(" and ")
-  elsif array.length == 3
+    return array.join(" and ") #adds 'and' between elements when given a 2-element array
+  elsif array.length == 3 #adds commas plus a final 'and' when given a 3-element array
   return array[0..-2].join(", ") + ", and " + array[-1] 
-  else array.length > 3
+  else array.length > 3 #correctly formats arrays of lengths greater than three
     return array[0..-2].join(", ") + ", and " + array[-1]
-end
+  end
 
 end
 
